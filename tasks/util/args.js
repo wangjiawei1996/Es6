9415f626c7c09ee90dc8ff74cedc8136259f4127
王嘉巍 <906789088@qq.com>
2018-10-12 18:33:17 +0800

    feat:js-foot

---
+++ tasks/util/args.js
@@ -0,0 +1,34 @@
+import yargs from 'yargs';
+
+const yargs = yargs
+
+  .option('production',{
+    boolean:true,
+    default:false,
+    describe:'min all script'
+  })
+
+  .option('watch',{
+    boolean:true,
+    default:false,
+    describe:'watch all script'
+  })
+
+  .option('verbose',{
+    boolean:true,
+    default:false,
+    describe:'log all script'
+  })
+
+  .option('sourcemaps',{
+    describe:'force the creation of sourcemaps'
+  })
+
+  .option('port',{
+    string:true,
+    default:8080,
+    describe:'server port'
+  })
+
+  .argv
+
