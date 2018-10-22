88c7e7882c80c33e864e34510625d6195e21bf4d
王嘉巍 <906789088@qq.com>
2018-10-22 23:03:02 +0800

    feat:lesson17

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,10 @@
 // import './class/lesson1';
 // import './class/lesson2';
 // import './class/lesson3';
-import 'babel-polyfill';
-import './class/lesson16';
+// import 'babel-polyfill';
+// import './class/lesson17';
+// import * as lesson from './class/lesson17'
+
+// console.log(lesson.A);
+import lesson17 from './class/lesson17'
+console.log(lesson17.A)
