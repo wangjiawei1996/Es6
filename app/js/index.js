7fc680eb0c74d9368d3439a0f4788faeb59fe1b6
王嘉巍 <906789088@qq.com>
2018-10-23 17:29:44 +0800

    feat:lottery.js-buliding

--- app/js/index.js
+++ app/js/index.js
@@ -1,10 +1,2 @@
-// import './class/lesson1';
-// import './class/lesson2';
-// import './class/lesson3';
-// import 'babel-polyfill';
-// import './class/lesson17';
-// import * as lesson from './class/lesson17'
-
-// console.log(lesson.A);
-import lesson17 from './class/lesson17'
-console.log(lesson17.A)
+ import 'babel-polyfill';
+ import Lottery from './lottery';
