2f3b23fc3b48f6f5a629d5cd91dcdb5132530ccc
王嘉巍 <906789088@qq.com>
2018-10-25 23:28:44 +0800

    fix:base-random

--- app/js/lottery/base.js
+++ app/js/lottery/base.js
@@ -177,4 +177,52 @@ class Base{
         $(self.cart_el).append(tpl);
         self.getTotal();
      }
+
+     getCount(){
+         let self = this;
+         let $active=$('.boll-list .btn-boll-active').length;
+         let count = self.computeCount(active,self.cur_play);
+         let range = self.computeBonus(active,self.cur_play);
+         let money = count*2;
+         let win1 = range[0]-money;
+         let win2 = range[1]-money;
+         let tpl;
+         let c1=(win1<0&&win2<0)?Math.abs(win1):win1;
+         let c2=(win1<0&&win2<0)?Math.abs(win2):win2;
+         if(count===0){
+             tpl=`您选了 <b class="red">${count}</b> 注，共 <b class="red">${count*2}</b> 元`
+         }else if(range[0]===range[1]){
+             tpl=`您选了 <b>${count}</b> 注，共 <b>${count*2}</b> 元  <em>若中奖，奖金：
+             <strong class="red">${range[0]}</strong> 元，
+             您将${win1>=0?'盈利':'亏损'}
+             <strong class="${win1>=0?'red':'green'}">${Math.abs(win1)} </strong> 元</em>`
+         }else{
+            tpl=`您选了 <b>${count}</b> 注，共 <b>${count*2}</b> 元  <em>若中奖，奖金：
+			<strong class="red">${range[0]}</strong> 至 <strong class="red">${range[1]}</strong> 元，
+			您将${(win1<0&&win2<0)?'亏损':'盈利'}
+			<strong class="${win1>=0?'red':'green'}">${c1} </strong>
+			至 <strong class="${win2>=0?'red':'green'}"> ${c2} </strong>
+			元</em>`
+         }
+         $('.sel_info').html(tpl);
+     }
+
+     /**
+      * getTotal [计算所有金额]
+      */
+    getTotal(){
+        let count=0;
+        $('.codelist li').each(function(index,item){
+            count+=$(item).attr(count)*1;
+        })
+        $('#count').text(count);
+        $('#money').text(count*2);
+    }
+    /**
+     * getRandom  [生成随机数]
+     * @param {*} num 
+     */
+    getRandom(num){
+        
+    }
 }
